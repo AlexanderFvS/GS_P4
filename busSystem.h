ef5c780609569e2dcc6c985962daf9a48235c950
AlexanderFvS <Alexander.FreiherrvonSeld@haw-hamburg.de>
2025-06-24 22:21:42 +0200

    Update busSystem.h

--- busSystem.h
+++ busSystem.h
@@ -23,6 +23,15 @@ typedef struct __attribute__((packed)) SCRATCH_PAD
 	
 } SCRATCH_PAD;
 
+// ROM befehle
+#define READ_ROM 0x33
+#define SKIP_ROM 0xCC
+#define MATCH_ROM 0x55
+#define SEARCH_ROM 0xF0
+// Funktions Befehle
+#define CONVERT_T 0x44
+#define READ_SCRATCHPAD 0xBE
+
 
 
 void writeHigh ();
