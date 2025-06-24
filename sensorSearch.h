dcae4466a09f8f67c3d768f9ddb6817b8b4a3308
AlexanderFvS <Alexander.FreiherrvonSeld@haw-hamburg.de>
2025-06-24 22:20:35 +0200

    Create sensorSearch.h

---
+++ sensorSearch.h
@@ -0,0 +1,14 @@
+#include "busSystem.h"
+#include "busSystem.h"
+#include "gp_io.h"
+
+
+
+
+
+int SensorSearch (ROM* romList, int maxSensors);
+
+int romSearch (ROM* rom);
+
+void readBitPair ();
+
