508664a9c932270c3ea54cedfe6cb1cc5d2c5c08
AlexanderFvS <Alexander.FreiherrvonSeld@haw-hamburg.de>
2025-06-28 14:48:52 +0200

    Update sensorSearch.h

--- sensorSearch.h
+++ sensorSearch.h
@@ -2,13 +2,17 @@
 #include "busSystem.h"
 #include "gp_io.h"
 
-
+#define MAX_SENSORS 10
 
 
 
 int SensorSearch (ROM* romList, int maxSensors);
 
 int romSearch (ROM* rom);
+
+int findDecision (int pos);
+
+void setDecision (int pos, int value);
 
 void readBitPair ();
 
